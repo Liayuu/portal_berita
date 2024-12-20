901009568bb78b375baef7a4d14cba963f376bc2
s-wira <55016501+s-wira@users.noreply.github.com>
2024-12-21 01:17:14 +0800

    fixing problem with absolute

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,21 +4,23 @@ const HeroSection: React.FC = () => {
   return (
     <div className="grid grid-cols-1 gap-8 my-12 px-4 md:px-8 lg:grid-cols-3">
       {/* Card Utama */}
-      <div className="lg:col-span-2 relative bg-gray-200 rounded-lg overflow-hidden" style={{ height: '500px', width: '900px' }}>
-        <div className="relative w-full h-full">
+      <div className="lg:col-span-2 overflow-hidden h-[500px] w-full relative">
+        <div className="pl-24 w-full h-full">
           <img
-            src="https://via.placeholder.com/900x500"
+            src="https://images.unsplash.com/photo-1532634922-8fe0b757fb13?q=80&w=1772&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Berita utama"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-lg"
           />
-          <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/2 bg-white p-4 rounded-lg shadow-lg" style={{ aspectRatio: "1 / 1", width: "300px", height: "300px" }}>
-            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2">People spend night on roofs and in trees after Ukraine dam breach</h2>
-            <p className="text-gray-700 mb-4 text-sm md:text-base">
-              Hundreds of thousands of people have been left without access to normal drinking water since the breach of the Kakhovka dam, Ukraine's President Volodymyr Zelensky has said.
-            </p>
-            <p className="text-gray-500 text-xs md:text-sm">Aug 03, 2023</p>
-          </div>
         </div>
+        <div className="absolute top-0 w-full h-full py-20 justify-center content-center">
+            <div className="bg-slate-50 p-4 shadow-lg max-w-sm h-full">
+              <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2 text-ellipsis overflow-hidden">People spend night on roofs and in trees after Ukraine dam breach</h2>
+              <p className="text-gray-700 mb-4 text-sm md:text-base text-ellipsis line-clamp-3 mt-2">
+                Hundreds of thousands of people have been left without access to normal drinking water since the breach of the Kakhovka dam, Ukraine's President Volodymyr Zelensky has said.
+              </p>
+              <p className="text-gray-500 text-xs md:text-sm">Aug 03, 2023</p>
+            </div>
+          </div>
       </div>
 
       {/* Card Kedua */}
