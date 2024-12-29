dadf1bf8516e582aec391c05775a3daa795b23e4
s-wira <55016501+s-wira@users.noreply.github.com>
2024-12-29 11:19:52 +0800

    Adding data fetching service

--- src/services/controllers/NewsController.tsx
+++ src/services/controllers/NewsController.tsx
@@ -3,16 +3,11 @@ import { getHomepageData, useAppDispatch } from ".."
 export default function NewsController() {
     const applicationDispatch = useAppDispatch();
 
-    const fetchNews = async () => {
+    const fetchHomepageNews = () => {
         applicationDispatch(
-            getHomepageData({
-                page: 1,
-                category: "1",
-                tag: "1",
-                writer: "1"
-            })
+            getHomepageData()
         )
     }
 
-    return { fetchNews }
+    return { fetchHomepageNews }
 }
